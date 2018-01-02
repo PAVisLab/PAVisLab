dce05f31ba1c3ef5625f4711baf3c934bcb350fd
varuneagle555 <varunroyeagle@outlook.com>
2018-01-02 16:53:45 -0700

    Adding AR

---
+++ pavis-ar/Assets/Scripts/WorldCursor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCursor : MonoBehaviour {
+
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        //Initializing the meshrenderer for the game object
+        meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //The location and direction of the gaze of the camera
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            //Display the cursor
+            meshRenderer.enabled = true;
+
+            //Move the cursor to the point where the raycast hit.
+            this.transform.position = hitInfo.point;
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+        else
+        {
+            //If the raycast did not hit a hologram, hide the cursor mesh
+            meshRenderer.enabled = false;
+        }
+	}
+}
